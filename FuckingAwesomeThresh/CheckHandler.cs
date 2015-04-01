593787a7438b38831ec1db11e7eacfb867d7ff2e
CptSparklesSharp <ahmed-1597@hotmail.com>
2015-04-01 16:09:15 +1300

    .

---
+++ FuckingAwesomeThresh/CheckHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace FuckingAwesomeThresh
+{
+    static class CheckHandler
+    {
+        public static Dictionary<SpellSlot, Spell> Spells = new Dictionary<SpellSlot, Spell>()
+        {
+            {SpellSlot.Q, new Spell(SpellSlot.Q, 1025)},
+            {SpellSlot.W, new Spell(SpellSlot.W, 950)},
+            {SpellSlot.E, new Spell(SpellSlot.E, 400)},
+            {SpellSlot.R, new Spell(SpellSlot.R, 400)}
+        };
+
+        public static int qTimer;
+
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+
+        public static void Init()
+        {
+            Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+            GameObject.OnCreate += GameObject_OnCreate;
+        }
+
+        static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender.Position.Distance(Player.Position) > 1200)
+                return;
+            if (sender.Name == "Thresh_Base_Q_stab_tar.troy")
+            {
+                qTimer = Environment.TickCount + 1500;
+            }
+        }
+
+        static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (!sender.IsMe)
+                return;
+            if (args.SData.Name.ToLower().Contains("summonerflash"))
+            {
+                var target = TargetSelector.GetTarget(400, TargetSelector.DamageType.Physical);
+                if (target == null)
+                    return;
+                var pos = target.Position.Extend(sender.Position, sender.Distance(target) + 200);
+                Spells[SpellSlot.E].Cast(pos);
+            }
+        }
+
+        public static String ThreshQBuff = "threshqfakeknockup";
+
+        public static bool HasAntiCc(this Obj_AI_Base u)
+        {
+            return (u.HasBuff("BlackShield") || u.HasBuff("SivirE") || u.HasBuff("NocturneShroudofDarkness"));
+        }
+
+        public static Obj_AI_Base GetQUnit()
+        {
+            return ObjectManager.Get<Obj_AI_Base>().FirstOrDefault(a => a.IsEnemy && a.Distance(ObjectManager.Player) < 3000 && a.CheckBuff(ThreshQBuff));
+        }
+
+        public static BuffInstance GetBuffData(this Obj_AI_Base u, String s)
+        {
+            return u.Buffs.FirstOrDefault(a => a.Name == s || a.DisplayName == s);
+        }
+
+        public static bool CheckBuff(this Obj_AI_Base u, String s)
+        {
+            return u.HasBuff(s) || u.HasBuff(s, true);
+        }
+    }
+}
---
+++ FuckingAwesomeThresh/ComboStateHandler.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace FuckingAwesomeThresh
+{
+    class ComboStateHandler
+    {
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+
+        public static void Combo()
+        {
+            if (CheckHandler.GetQUnit() != null || CheckHandler.qTimer > Environment.TickCount)
+                return;
+
+            var target = TargetSelector.GetTarget(2000, TargetSelector.DamageType.Magical);
+
+            if (!target.IsValidTarget() || target.HasAntiCc())
+                return;
+
+            if (CheckHandler.Spells[SpellSlot.R].IsReady() &&
+                target.IsValidTarget(CheckHandler.Spells[SpellSlot.R].Range) && Player.Position.GetEnemiesInRange(CheckHandler.Spells[SpellSlot.R].Range).Where(a => a.IsValidTarget()).ToList().Count >= 2)
+            {
+                CheckHandler.Spells[SpellSlot.R].Cast();
+            }
+
+            if (CheckHandler.Spells[SpellSlot.E].IsReady() &&
+                target.IsValidTarget(CheckHandler.Spells[SpellSlot.E].Range))
+            {
+                var ally =
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .FirstOrDefault(ally2 => ally2.IsAlly && ally2.Distance(Player) < 2000);
+                if (ally == null)
+                {
+                    Pull(target);
+                    return;
+                }
+                if (ally.Distance(Player) < ally.Distance(target))
+                {
+                    Push(target);
+                    return;
+                }
+                    Pull(target);
+            }
+
+            if (CheckHandler.Spells[SpellSlot.Q].IsReady() &&
+                target.IsValidTarget(CheckHandler.Spells[SpellSlot.Q].Range))
+            {
+                CheckHandler.Spells[SpellSlot.Q].Cast(target);
+            }
+        }
+
+        public static void EngageCombo()
+        {
+            if (CheckHandler.GetQUnit() != null || CheckHandler.qTimer > Environment.TickCount)
+                return;
+
+            var target = TargetSelector.GetTarget(2000, TargetSelector.DamageType.Magical);
+
+            if (!target.IsValidTarget() || target.HasAntiCc())
+                return;
+
+            if (GetBestAllyEngage() != null)
+            {
+                CheckHandler.Spells[SpellSlot.W].Cast(GetBestAllyEngage().Position);
+            }
+
+            if (CheckHandler.Spells[SpellSlot.R].IsReady() &&
+                target.IsValidTarget(CheckHandler.Spells[SpellSlot.R].Range) && Player.Position.GetEnemiesInRange(CheckHandler.Spells[SpellSlot.R].Range).Where(a => a.IsValidTarget()).ToList().Count >= 2)
+            {
+                CheckHandler.Spells[SpellSlot.R].Cast();
+            }
+
+            if (CheckHandler.Spells[SpellSlot.E].IsReady() &&
+                target.IsValidTarget(CheckHandler.Spells[SpellSlot.E].Range))
+            {
+                var ally =
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .FirstOrDefault(ally2 => ally2.IsAlly && ally2.Distance(Player) < 1000);
+                if (ally == null)
+                {
+                    Pull(target);
+                    return;
+                }
+                if (ally.Distance(Player) < ally.Distance(target))
+                {
+                    Push(target);
+                    return;
+                }
+                Pull(target);
+            }
+
+            if (CheckHandler.Spells[SpellSlot.Q].IsReady() &&
+                target.IsValidTarget(CheckHandler.Spells[SpellSlot.Q].Range))
+            {
+                CheckHandler.Spells[SpellSlot.Q].Cast(target);
+            }
+        }
+
+        public static void Pull(Obj_AI_Base target = null)
+        {
+            if (target == null)
+            {
+                target = TargetSelector.GetTarget(400, TargetSelector.DamageType.Physical);
+                if (target == null)
+                    return;
+            }
+            var pos = target.Position.Extend(Player.Position, Player.Distance(target) + 200);
+            CheckHandler.Spells[SpellSlot.E].Cast(pos);
+        }
+
+        public static void Push(Obj_AI_Base target = null)
+        {
+            if (target == null)
+            {
+                target = TargetSelector.GetTarget(400, TargetSelector.DamageType.Physical);
+                if (target == null)
+                    return;
+            }
+            var pos = target.Position.Extend(Player.Position, Player.Distance(target) - 200);
+            CheckHandler.Spells[SpellSlot.E].Cast(pos);
+        }
+
+        public static void FlashPull(Obj_AI_Base target = null)
+        {
+            if (target == null)
+            {
+                target = TargetSelector.GetTarget(400, TargetSelector.DamageType.Physical);
+                if (target != null)
+                {
+                    Pull(target);
+                    return;
+                }
+                target = TargetSelector.GetTarget(800, TargetSelector.DamageType.Physical);
+                if (target == null)
+                    return;
+            }
+            Player.Spellbook.CastSpell(Player.GetSpellSlot("summonerflash"), target.Position);
+        }
+
+        private static Obj_AI_Hero GetBestAllyEngage()
+        {
+            var distance = 0;
+            var priorityNo = 0;
+            Obj_AI_Hero selectedAlly = null;
+            foreach (var ally in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsAlly && hero.Distance(Player) < CheckHandler.Spells[SpellSlot.W].Range))
+            {
+               var priority = Program.Config.Item("priority" + ally.ChampionName).GetValue<Slider>().Value;
+                if (priority > priorityNo || priority == priorityNo && distance < Player.Distance(ally))
+                {
+                    distance = (int) Player.Distance(ally);
+                    priorityNo = priority;
+                    selectedAlly = ally;
+                }
+            }
+            return selectedAlly;
+        }
+
+        private static Obj_AI_Hero GetBestAllyForShield()
+        {
+            return null;
+
+        }
+    }
+}
